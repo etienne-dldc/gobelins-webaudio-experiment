74513903dd036b0f850d24de37cc9454f2789602
Etienne Dldc <etienne@twik-labs.fr>
2015-10-02 11:15:17 +0200

    gulp ok

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,44 @@
 'use strict';
-​
-var gulp                = require('gulp');
-var sass                = require('gulp-sass');
-var babel               = require("gulp-babel");
-var plumber             = require('gulp-plumber');
-var browserSync         = require('browser-sync').create();
-var autoprefixer        = require('gulp-autoprefixer');
-var minifyCss           = require('gulp-minify-css');
-var filter              = require('gulp-filter');
-var filter              = require('gulp-rimraf');
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var babel = require("gulp-babel");
+var plumber  = require('gulp-plumber');
+var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
+var minifyCss = require('gulp-minify-css');
+var filter = require('gulp-filter');
+var clean = require('gulp-clean');
 
 /*
  * Config
  */
- // Path
 var src = {
   path 	  : 'app',
   styles 	: ['app/styles/**/*.scss'],
   scripts : 'app/scripts/**/*.js',
   vendor	: 'app/vendor',
-  assets  : 'app/assets'
+  assets  : 'app/assets/**/*'
 };
 var dist = {
   path    : 'app/public',
   styles 	: 'app/public/styles',
   scripts : 'app/public/scripts',
   vendor	: 'app/public/vendor',
-  assets  : 'app/puclic'
+  assets  : 'app/public'
 };
-// Autiprefix
-var autoPrefixerBrowers = [
-	'chrome >             = 34',
-	'safari >             = 7',
-	'opera >              = 23',
-	'ios >                = 7',
-	'android >            = 4.0',
-	'bb >                 = 10',
-	'ie >                 = 10',
-	'ie_mob >             = 10',
-	'ff >                 = 30'
-];
-​
+
 /*
  * Server
  */
-gulp.task('server', ['styles'], function() {
-​
-    browserSync.init({
-        server: "./public"
-    });
-​
+gulp.task('server', ['clean', 'styles', 'scripts', 'assets'], function() {
+
+  browserSync.init({
+      server: "./app/public"
+  });
+
   gulp.watch(src.style, ['styles']);
+  gulp.watch(src.scripts, ['scripts']).on('change', browserSync.reload);
   gulp.watch(src.assets, ['assets']).on('change', browserSync.reload);
 });
 
@@ -58,37 +46,41 @@ gulp.task('server', ['styles'], function() {
  * Clean 'dist' folder
  */
 gulp.task('clean', function() {
-	gulp.src(dist.path, {read: false}).pipe(rimraf());
+	gulp.src(dist.path, {read: false})
+    .pipe(clean());
 });
-​
+
 /*
  * Copy 'assets'
  */
 gulp.task('assets', function() {
-  gulp.src()
+  gulp.src( src.assets )
     .pipe( gulp.dest(dist.assets) );
 });
-​
+
 /*
  * Scripts
  */
 gulp.task("scripts", function () {
   return gulp.src( src.scripts )
-    .pipe(babel())
+    .pipe( babel() )
     .pipe( gulp.dest(dist.scripts) );
 });
-​
+
 /*
  * Compile SCSS
  */
 gulp.task('styles', function () {
-  gulp.src(scr.styles)
+  gulp.src(src.styles)
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer(autoPrefixerBrowers))
+    .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+    }))
     .pipe(minifyCss({compatibility: 'ie8'}))
     .pipe(gulp.dest( dist.styles ))
     .pipe(browserSync.stream());
 });
-​
-gulp.task('default', ['styles', 'server']);
+
+gulp.task('default', ['server']);
